4cf3ae74a884fa5d91b21d98818e1cc48da51415
mervebirsinnn <mervebr34@outlook.com>
2019-02-19 15:58:25 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,15 +1,15 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://rfid-proje.firebaseio.com/', None)
-result = firebase.get('/admin','Merve Birsin')
+result = firebase.get('/admin','Deniz Ak')
 id="13 13 250 42 208"
 print(type(result))
 print(type(id))
 if(id==result):
-    print("Merve giris yaptı")
+    print("Deniz" giris yaptı")
     print(result)
 else:
     print("esit degil")
-    result1=firebase.get('/admin','Feride Ünlü')
+    result1=firebase.get('/admin','Eren Yazıcı')
     #if(id==result1)
-    print("Feride giris yaptı")
-    {'1': 'John Doe'}
+    print("Eren giris yaptı")
+    
